a62ce7fb94038cb2202191251140e4180241f943
deyanamika <78485532+deyanamika@users.noreply.github.com>
2023-02-18 15:17:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I am Anamika\n";
+  cout << "I am Anamika Dey\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
